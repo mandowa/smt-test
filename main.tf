2c1ac5c47523254a9413da3f517da4ac50beed82
Kim Liu <kimlcs@SMT-KimL-A2.local>
2021-01-19 16:13:18 +0800

    add RAM with TGW

--- main.tf
+++ main.tf
@@ -3,8 +3,8 @@ module "vpc" {
   source = "./modules/vpc"
 
   name = "Terraform_managed_VPC"
-  #cidr = "10.156.176.0/20"
-  cidr = var.cidr
+  cidr = "10.156.176.0/20"
+  #cidr = var.cidr
 
   azs              = ["ap-northeast-1a", "ap-northeast-1c","ap-northeast-1d"]
   #DMZ Subnets
@@ -30,12 +30,43 @@ module "vpc" {
   }
 }
 
-# data "aws_ram_resource_share" "tgw" {
-#     provider = aws.main
-#     name = "Sharing the PACI TGW with all the TCC Production accounts"
-#     resource_owner = "SELF"
-# }
+data "aws_ram_resource_share" "tgw" {
+    provider = aws.main
+    #name = "Sharing the Prod TGW with all the TCC Production accounts"
+    name = "Terraform_TGW_Sharing"
+    resource_owner = "SELF"
+}
+
+resource "aws_ram_principal_association" "prod" {
+    provider = aws.main
+    principal = "420009094734"
+    resource_share_arn = data.aws_ram_resource_share.tgw.arn
+
+}
+data "aws_ec2_transit_gateway" "main" {
+      filter {
+             name = "owner-id"
+            values = ["981045337300"]
+      }
+    depends_on = [aws_ram_principal_association.prod]
+}
+
+resource "aws_ec2_transit_gateway_vpc_attachment" "test" {
+  subnet_ids                                      = module.vpc.public_subnets
+  transit_gateway_id                              = data.aws_ec2_transit_gateway.main.id
+  vpc_id                                          = module.vpc.vpc_id
+  transit_gateway_default_route_table_propagation = false
+  tags = {
+    Name = "TGW-Prod"
+    Side = "creator"
+  }
+}
+
 
 # resource "aws_ram_resource_share_accepter" "receiver_accept" {
-#   share_arn = data.aws_ram_resource_share.tgw.arn
+#     share_arn = data.aws_ram_resource_share.tgw.arn
+# }
+
+# output "TGW_arn" {
+#   value = data.aws_ram_resource_share.tgw.arn
 # }
