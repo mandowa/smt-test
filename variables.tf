d9f63fe468acccecff6a7ef3027a66bcb6032dd3
Kim Liu <kimlcs@SMT-KimL-A2.local>
2021-01-19 10:48:42 +0800

    add subnets variables

--- variables.tf
+++ variables.tf
@@ -3,3 +3,21 @@ variable "cidr" {
   type        = string
   default     = "0.0.0.0/0"
 }
+
+variable "cloudnative_subnets" {
+  description = "Must be of equal length to the corresponding IPv4 subnet list"
+  type        = list(string)
+  default     = []
+}
+
+variable "trusted_subnets" {
+  description = "Must be of equal length to the corresponding IPv4 subnet list"
+  type        = list(string)
+  default     = []
+}
+
+variable "dmz_subnets" {
+  description = "Must be of equal length to the corresponding IPv4 subnet list"
+  type        = list(string)
+  default     = []
+}
